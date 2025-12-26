850b8aecf2160bd333d014246802d4d3f313c38c
snehal-chikane <snehalchikane727@gmail.com>
2025-12-26 10:48:12 +0000

    Creating main file

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
 //main.tf file
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "snehal" {
+  ami = "ami-00ca570c1b6d79f36"
+  subnet_id = "subnet-0b83fcae4efe7c8f9"
+  instance_type = "t2.micro"
+}
+
